2ba65a3de538443948d04e0755bfb14bb90b018f
sahitoattique <sahitoattique@gmail.com>
2025-04-23 15:22:06 +0500

    Update title and favico n2

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,30 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "The Suit",
   description: "Threads and Hands",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", type: "image/x-icon" },
+    ],
+    apple: "/apple-touch-icon.png",
+    other: [
+      {
+        rel: "android-chrome",
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        rel: "android-chrome",
+        url: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
 };
+
 
 export default function RootLayout({
   children,
