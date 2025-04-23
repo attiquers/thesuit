3129c8763ec6aa3db76f0b804079ddf9ee6f1aaa
sahitoattique <sahitoattique@gmail.com>
2025-04-23 16:12:19 +0500

    Added mail submission

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import "./page.css";
 
-
 export default function Home() {
   return (
     <div className="page-container">
@@ -23,61 +22,61 @@ export default function Home() {
         playsInline
       />
 
-
-
       <div className="wrapper absolute">
-        <div id="glass" className="flex flex-col gap-10 ">
-          <div className="text-center ">
-            <h1 className="text-4xl md:text-5xl libre-baskerville-regular">thesuit</h1>
-            <span className="text-[10px] md:text-sm tracking-widest flex justify-center gap-2"> 
+        <div id="glass" className="flex flex-col gap-10">
+          <div className="text-center">
+            <h1 className="text-5xl md:text-6xl libre-baskerville-regular">thesuit</h1>
+            <span className="text-xs md:text-sm tracking-widest md:tracking-wide flex justify-center gap-2">
               <span>threads</span>
               <span>and</span>
               <span>hands</span>
             </span>
           </div>
 
-          <div className="flex flex-col gap-4 ">
+          <div className="flex flex-col gap-4">
             <div className="text-5xl text-center tracking-[12] md:text-7xl md:tracking-[28]">COMING SOON</div>
-            <form action="" method="post" 
-              className="flex flex-col md:flex-row items-center justify-center w-full  mx-auto text-sm gap-2 "
-              >
-              <input 
-                type="email" 
-                name="" 
-                id="" 
-                className="bg-white text-black " 
-                placeholder="Email here" 
-                style={{ 
-                  paddingTop: '8px', 
-                  paddingBottom: '8px', 
-                  paddingLeft:'20px',
-                  borderRadius: '20px', 
-                }} 
+            <form
+              action="https://formsubmit.co/attiquerehmansahito@gmail.com"
+              method="POST"
+              className="flex flex-col md:flex-row items-center justify-center w-full mx-auto text-sm gap-2"
+            >
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="bg-white text-black min-w-[250px] text-xs md:text-base md:min-w-sm"
+                placeholder="Email here"
+                required
+                style={{
+                  paddingTop: '8px',
+                  paddingBottom: '8px',
+                  paddingLeft: '20px',
+                  borderRadius: '20px',
+                }}
               />
-
-
-              <button 
-                type="submit" 
-                className="bg-[#531518] text-white" 
-                style={{ 
-                  paddingTop: '10px', 
-                  paddingBottom: '10px', 
-                  paddingLeft:'20px',
-                  paddingRight:'20px',
-                  borderRadius: '20px', 
-                }} 
-              > 
-              Notify Me
+              <button
+                type="submit"
+                className="bg-[#531518] text-white hover:opacity-80 hover:cursor-pointer"
+                style={{
+                  paddingTop: '10px',
+                  paddingBottom: '10px',
+                  paddingLeft: '20px',
+                  paddingRight: '20px',
+                  borderRadius: '20px',
+                }}
+              >
+                Notify Me
               </button>
-
-
-
             </form>
           </div>
 
-          <div className=" flex gap-2">
-                  <Image src="/fb.png" alt="fblogo" width={30} height={30}/>
-                  <Image src="/insta.png" alt="fblogo" width={30} height={30}/>
+          <div className="flex gap-2">
+          <a href="https://www.instagram.com/thesu.it/" target="_blank" rel="noopener noreferrer">
+              <Image src="/fb.png" alt="Facebook Logo" width={30} height={30} />
+            </a>
+            <a href="https://www.instagram.com/thesu.it/" target="_blank" rel="noopener noreferrer">
+              <Image src="/insta.png" alt="Instagram Logo" width={30} height={30} />
+            </a>
           </div>
         </div>
       </div>
